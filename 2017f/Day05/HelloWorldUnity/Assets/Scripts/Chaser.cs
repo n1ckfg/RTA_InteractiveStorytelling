847b5144cb9eb3958e51285dac37a22bb4769e09
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-05 21:04:54 -0400

    day 5

---
+++ 2017f/Day05/HelloWorldUnity/Assets/Scripts/Chaser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, target.position, speed);	
+	}
+
+}
